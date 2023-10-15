b0e2b7a7777dededb1ecc400a82eaa71ed6eaf30
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-10-15 19:03:07 -0400

    upload local project to git

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Tharindu N.",
+		email: "emmanuel.mayowa.m@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/EmmanuelMoshood",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Cloud Devops Engineer, and a result oriented IT specialist.",
+		description:
+			"As a devops engineer with expertise in demonstrated history working in IT and skilled in configuting and managing automated CI/CD pipeplines. I have experience in building scalable, secure and reliable web applications using native cloud technologies. I enjoy solving complex problems and learning new skills. My skills include: Terraform, Jenkins, UNIX/Linux, PowerShell, AWS, Azure DevOps, GitHub actions, GitHub, Kubernetes, AWS Services (CloudFormation, EC2, ECS, S3, Route 53, VPC, ELB, EFS, Transit Gateways, SSM) Nginx, Sonarqube, Docker, Tomcat, etc.",
+	},
+
+	about: {
+		title: "With a career spanning over 5 years, Emmanuel has worked on a number of projects",
+		description:
+			"Emmmanuel's background includes working on Cloud Native Technology, Infrastructure as code, Continuous Integration/Continous Delivery pipelines, Microservices and Containerizing Multi-Tier Application using Devops/SRE best Practices.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
